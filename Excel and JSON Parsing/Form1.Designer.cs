a564d3e22e62494a0f0fcd3e7cc17455f6084016
Isha Kansal <106754674+IshaKansal-25@users.noreply.github.com>
2024-01-03 11:16:26 +0530

    Add project files.

---
+++ Excel and JSON Parsing/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Excel_Parsing_and_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.saveJSONbtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(550, 29);
+            this.title.TabIndex = 0;
+            this.title.Text = "Parse Excel file and download in JSON format";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFileBtn.Location = new System.Drawing.Point(1202, 95);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(122, 67);
+            this.openFileBtn.TabIndex = 1;
+            this.openFileBtn.Text = "Open File";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // saveJSONbtn
+            // 
+            this.saveJSONbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveJSONbtn.Location = new System.Drawing.Point(1202, 232);
+            this.saveJSONbtn.Name = "saveJSONbtn";
+            this.saveJSONbtn.Size = new System.Drawing.Size(122, 80);
+            this.saveJSONbtn.TabIndex = 2;
+            this.saveJSONbtn.Text = "Save JSON file";
+            this.saveJSONbtn.UseVisualStyleBackColor = true;
+            this.saveJSONbtn.Click += new System.EventHandler(this.saveJSONbtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Excel file | *.xlsx; | JSON File | *.json; | All files | *.*;";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "JSON File | *.json;";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1135, 593);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1412, 671);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.saveJSONbtn);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.title);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel Parsing to DataGridView and downloading JSON";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.Button saveJSONbtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Excel and JSON Parsing/Form1.cs
@@ -0,0 +1,136 @@
+﻿//using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Excel;
+
+namespace Excel_Parsing_and_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        DataSet data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                Load_Excel(fileName);
+            }
+        }
+
+        private void saveJSONbtn_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                Save_JSON();
+        }
+
+        public void Load_Excel(string path)
+        {
+            if (path.EndsWith(".xlsx"))
+            {
+                try
+                {
+                    using (var fileStream = File.Open(path, FileMode.Open, FileAccess.Read))
+                    {
+                        IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(fileStream);
+                        reader.IsFirstRowAsColumnNames = true;
+                        data = reader.AsDataSet();
+                        dataGridView1.DataSource = data.Tables[0];
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message.ToString());
+                }
+            }
+
+            else if (path.EndsWith(".json"))
+            {
+                var jsonFile = File.ReadAllText(path);
+                dataGridView1.DataSource = JsonConvert.DeserializeObject<DataTable>(jsonFile);
+            }
+           
+        }
+
+        public void Save_JSON()
+        {
+            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                Dictionary<string, object> rowData = new Dictionary<string, object>();
+
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    rowData[column.Name] = row.Cells[column.Index].Value;
+                }
+
+                data.Add(rowData);
+            }
+
+            var jsonFile = JsonConvert.SerializeObject(data, Formatting.Indented);
+            String fileName = saveFileDialog1.FileName;
+            File.WriteAllText(fileName, jsonFile);
+        } 
+    }
+}
+
+
+
+
+/*
+ * 
+ * Using Interop :
+                    Excel.Application excelApp = new Excel.Application();
+                    Excel.Workbook excelWB = excelApp.Workbooks.Open(path);
+                    Excel.Worksheet excelWS = excelWB.Sheets[1];
+
+                    DataTable data = new DataTable();
+
+                    Excel.Range excelRange = excelWS.UsedRange;
+                    for (int row = 1; row <= excelRange.Rows.Count; row++)
+                    {
+                        DataRow dataRow = data.NewRow();
+                        for (int col = 1; col <= excelRange.Columns.Count; col++)
+                        {
+                            if (row == 1)       
+                            {
+                                string headerData = (excelWS.Cells[row, col] as Excel.Range).Value;
+                                data.Columns.Add(headerData);
+                            }
+                            else
+                            {
+                                dataRow[col - 1] = Convert.ToString((excelWS.Cells[row, col] as Excel.Range).Value);
+                            }
+                        }
+                        if (row > 1)
+                        {
+                            data.Rows.Add(dataRow);
+                        }
+
+                        foreach (DataRow row_ in data.Rows)
+                        {
+                            Employee employee = new Employee
+                            {
+                                Id = Convert.ToInt32(row_["Id"]),
+                                Name = row_["Name"].ToString(),
+                                Age = Convert.ToInt32(row_["Age"]),
+                                Designation = row_["Designation"].ToString(),
+                            };
+                            employees.Add(employee);
+                        }
+                    }
+                    dataGridView1.DataSource = data;
+                    */
